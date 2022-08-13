4679253fee7dd59e5b8eb34b2b4210bed23d7613
physphase <physphase@gmail.com>
2022-08-13 18:53:47 +0200

    Create toggleSuspend

--- PauseGame.sh
+++ PauseGame.sh
@@ -1,2 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 #This script allows to let the user pause or suspend a program or game when going AFK.
+#Created by physphase, August 13th 2022
+
+function toggleSuspend()
+{
+    PID=$(xprop -id `xdotool getwindowfocus` | grep '_NET_WM_PID' | grep -oE '[[:digit:]]*$')
+    status="$(ps aux | grep ${PID} | awk '{print $8}')"
+    status=${status:0:1}
+    
+    case $status in
+        S)
+            kill -STOP "${PID}"
+            ;;
+        T)
+            kill -CONT "${PID}"
+            ;;
+        *)
+            echo "Invalid code"
+    esac
+ return 0
+}
+
+toggleSuspend
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GreenGaming
-Tools to decrease PC power usage when afk
+Tool to decrease PC power usage when afk by suspending a single program with a keybind.
+
+for Nvidia: check gpu usage by using the command "nvidia-smi". This shows GPU power usage and the load percentage.
+for AMD: Try checking the GPU utilization by using the commands "amdconfig --odgc" and "amdconfig --odgt". I do not have an AMD graphics card, so I could not test these commands.
